daf2c030dd78958c4579a7fd627d1792c4b933e8
nazmul98 <coder.nazmul@gmail.com>
2021-07-30 20:15:49 -0400

    control flow added

---
+++ Program/ifelse.py
@@ -0,0 +1,17 @@
+a = 100
+b = 200
+c = 300
+if a > b and a > c:
+    print(f"{a} is biggest number!")
+elif b > a and b > c:
+    print(f"{b} is biggest number!")
+else: 
+    print(f"{c} is a biggest number!")
+
+# Output: 300 is a biggest number!
+
+a, b = 100, 200
+print(f"{a} > {b} or {a} < {b}") if a > b or a < b else print(f"{a} = {b}")
+# Output: 100 > 200 or 100 < 200
+
+print(a) if a > b else print(b) if b > a else print(a, b) # Output: 200
