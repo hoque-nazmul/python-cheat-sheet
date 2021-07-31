19e3641ce35f10ad728e983e241ec827bd1f4469
nazmul98 <coder.nazmul@gmail.com>
2021-07-31 18:17:19 -0400

    for loop added

---
+++ Program/for.py
@@ -0,0 +1,6 @@
+for item in range(0, 11, 2):
+    print(item, end=' ')  # Print Even: 0 2 4 6 8 10
+
+# Create a List Using For
+li = [item for item in range(1, 5)]
+print(li) # Output: [1, 2, 3, 4]
