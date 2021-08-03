87d4453764490482df8d63eccfa27fbbd5fd781d
nazmul98 <coder.nazmul@gmail.com>
2021-08-03 10:20:44 -0400

    buitin function list added

---
+++ Program/builtin.py
@@ -0,0 +1,45 @@
+# Math Relavant Built-in Function
+sum()
+divmod()
+abs()
+round()
+pow()
+min()
+max()
+range()
+format()
+eval()
+
+# Function for Number System
+int()
+bin()
+oct()
+hex()
+
+# Data type Functions
+str()
+int()
+float()
+list()
+tuple()
+set()
+dict()
+
+# Fuctions for Traversing Iterable Object
+all()
+any()
+map()
+filter()
+sorted()
+reversed()
+len()
+enumerate()
+zip()
+slice()
+min(), max()
+
+# Access the Object property 
+getattr()
+setattr()
+delattr()
+hasattr()
